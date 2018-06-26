89b7efe8d56c58f8584bdbf1c486eeb194e6f9a5
Patrick Coston <patcoston@gmail.com>
2018-06-25 21:29:42 -0400

    Arrays img pix and vectors initialized

--- index.js
+++ index.js
@@ -1,24 +1,79 @@
-var Jimp = require("jimp");
+let Jimp = require('jimp');
  
-Jimp.read("lenna.png", function (err, image) {
+Jimp.read('input.png', function (err, image) {
     if (err) throw err;
-    var width = image.bitmap.width
-    var height = image.bitmap.height;
+    let width = image.bitmap.width
+    let height = image.bitmap.height;
     // create 2D array for image
-    var img = new Array(width);
-    for (var i = 0; i < width; i++) {
+    let img = new Array(width);
+    for (let i = 0; i < width; i++) {
         img[i] = new Array(height);
     }
-    image.scan(0, 0, image.bitmap.width, image.bitmap.height, function (x, y, idx) {
+    let pix = new Array(width * height);
+    let dir = new Array(8);
+    let n = 0;
+    for (let x = -1; x <= 1; x++) {
+        for (let y = -1; y <= 1; y++) {
+            if (x || y) {
+                dir[n++] = { x: x, y: y };
+            }
+        }
+    }
+    function getPixVectors(pix) {
+        let n = 0;
+        for (let v = 0; v < 8; v++) {
+            let x = pix.x + dir[v].x;
+            let y = pix.y + dir[v].y;
+            if ((x >= 0) && (y >= 0) && (x < width) && (y < height)) {
+                pix.vectors[n++] = {
+                    x: x,
+                    y: y,
+                }
+            }
+        }
+        pix.vectorCount = n;
+    }
+    let pixNum = 0;
+    image.scan(0, 0, width, height, function (x, y, idx) {
         // x, y is the position of this pixel on the image
         // idx is the position start position of this rgba tuple in the bitmap Buffer
-        var R = this.bitmap.data[idx];
-        var G = this.bitmap.data[idx + 1];
-        var B = this.bitmap.data[idx + 2];
-        var A = this.bitmap.data[idx + 3];
-
-        var hex = Jimp.rgbaToInt(255, 255, 255, A);
-        image.setPixelColor(hex, x, y);
+        let R = this.bitmap.data[idx];
+        let G = this.bitmap.data[idx + 1];
+        let B = this.bitmap.data[idx + 2];
+        let A = this.bitmap.data[idx + 3];
+        img[x][y] = pixNum;
+        pix[pixNum] = {
+            R: R,
+            G: G,
+            B: B,
+            A: A,
+            x: x,
+            y: y,
+            groupID: 0,
+            swapped: false,
+            distToCenter: 0,
+            vectors: new Array(8),
+            vectorCount: 0,
+        }
+        getPixVectors(pix[pixNum]);
+        pixNum++;
     });
-    image.write("lena-white.png"); // save
+    let gen = 0;
+    let generations = 10;
+    while(gen < generations) {
+        image.scan(0, 0, width, height, function (x, y, idx) {
+            // x, y is the position of this pixel on the image
+            // idx is the position start position of this rgba tuple in the bitmap Buffer
+            let n = img[x][y];
+            let p = pix[n];
+            let R = p.R;
+            let G = p.G;
+            let B = p.B;
+            let A = p.A;
+            let hex = Jimp.rgbaToInt(R, G, B, A);
+            image.setPixelColor(hex, x, y);
+        });
+        image.write(`gen${gen}.png`); // save
+        gen++;
+    }
 });
