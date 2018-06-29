a9ac267d0fac526cbb1546d7ad74c9ce8a228ab6
Patrick Coston <patcoston@gmail.com>
2018-06-28 22:48:17 -0400

    Calc center of each color group

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ let Jimp = require('jimp');
 Jimp.read('a.png', function (err, image) {
     if (err)
         throw err;
+    let size = 32; // 1 2 4 8 16 32 64 128 256
+    let blocks = 256 / size; // per Red, Green or Blue
+    let colorGroup = new Array(blocks * blocks * blocks);
     let width = image.bitmap.width;
     let height = image.bitmap.height;
     // create 2D array for image
@@ -10,10 +13,16 @@ Jimp.read('a.png', function (err, image) {
     for (let i = 0; i < width; i++) {
         img[i] = new Array(height);
     }
+    console.log('Init colorGroup array ' + colorGroup.length);
+    for (let i = 0; i < colorGroup.length; i++) {
+        colorGroup[i] = {
+            center: { R: 0, G: 0, B: 0 },
+            pixIndex: []
+        };
+    }
     let pix = new Array(width * height);
-    let dir = new Array(8);
     let n = 0;
-    dir = [
+    let dir = [
         { x:  0, y: -1 }, // N
         { x:  1, y: -1 }, // NW
         { x:  1, y:  0 }, // W
@@ -39,8 +48,6 @@ Jimp.read('a.png', function (err, image) {
     }
     let pixNum = 0;
     image.scan(0, 0, width, height, function (x, y, idx) {
-        let size = 64; // 1 2 4 8 16 32 64 128 256
-        let blocks = 256 / size; // per Red, Green or Blue
         // x, y is the position of this pixel on the image
         // idx is the position start position of this rgba tuple in the bitmap Buffer
         let R = this.bitmap.data[idx];
@@ -51,7 +58,6 @@ Jimp.read('a.png', function (err, image) {
         let G1 = Math.abs(Math.round(G / size - 0.5));
         let B1 = Math.abs(Math.round(B / size - 0.5));
         let group = R1 + blocks * (G1 + blocks * B1);
-        console.log(R, G, B, R1, G1, B1, group);
         img[x][y] = pixNum;
         pix[pixNum] = {
             R: R,
@@ -66,15 +72,41 @@ Jimp.read('a.png', function (err, image) {
             vectorCount: 0,
             group: group,
         };
-        getPixVectors(pix[pixNum]);
+        colorGroup[group].pixIndex.push(pixNum);
         pixNum++;
     });
     let gen = 0;
     let generations = 5;
     while (gen < generations) {
+        // Calculate center of each color group
+        for (let i = 0; i < colorGroup.length; i++) {
+            if (colorGroup[i].pixIndex.length > 0) {
+                let R = 0;
+                let G = 0;
+                let B = 0;
+                let count = colorGroup[i].pixIndex.length;
+                for (let j = 0; j < count; j++) {
+                    let n = colorGroup[i].pixIndex[j];
+                    let p = pix[n];
+                    R += p.R;
+                    G += p.G;
+                    B += p.B;
+                }
+                colorGroup[i].center = {
+                    R: Math.round(R / count),
+                    G: Math.round(G / count),
+                    B: Math.round(B / count),
+                }
+            }
+        }
+        // TODO: Calculate distance to each vector then sort by distance
         image.scan(0, 0, width, height, function (x, y, idx) {
-            // x, y is the position of this pixel on the image
-            // idx is the position start position of this rgba tuple in the bitmap Buffer
+            let n = img[x][y];
+            let p = pix[n];
+            getPixVectors(pix[n]);
+
+        });
+        image.scan(0, 0, width, height, function (x, y, idx) {
             let n = img[x][y];
             let p = pix[n];
             let R = p.R;
